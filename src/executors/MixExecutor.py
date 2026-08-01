e2870f4624bc7c824833f5d0d7f72a50168ecd9c
Mihrace <mihracetabiloglu@gmail.com>
2026-08-01 18:19:13 +0300

    update

---
+++ src/executors/MixExecutor.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import cv2
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
+
+from sdks.novavision.src.media.image import Image
+from sdks.novavision.src.base.component import Component
+from sdks.novavision.src.helper.executor import Executor
+from components.Package.src.models.PackageModel import PackageModel
+from components.Package.src.utils.response import build_response_mix
+
+
+class ExecutorTwo(Component):
+    def __init__(self, request, bootstrap):
+        super().__init__(request, bootstrap)
+        self.request.model = PackageModel(**(self.request.data))
+        self.image_one = self.request.get_param("inputImageOne")
+        self.image_two = self.request.get_param("inputImageTwo")
+        self.method = self.request.get_param("demoDependentDropdown")
+
+        if self.method == "OptionA":
+            self.val_int = self.request.get_param("optionAIntegerField")
+            self.val_bool = self.request.get_param("optionABoolField")
+        else:
+            self.val_float = self.request.get_param("optionBFloatField")
+            self.val_string = self.request.get_param("optionBStringField")
+
+    @staticmethod
+    def bootstrap(config: dict) -> dict:
+        return {}
+
+    def process(self, img_one, img_two):
+        height, width = img_one.shape[:2]
+        img_two_resized = cv2.resize(img_two, (width, height))
+        blended_image = cv2.addWeighted(img_one, 0.5, img_two_resized, 0.5, 0)
+        difference_image = cv2.absdiff(img_one, img_two_resized)
+        return blended_image, difference_image
+
+    def run(self):
+        img_one = Image.get_frame(img=self.image_one, redis_db=self.redis_db)
+        img_two = Image.get_frame(img=self.image_two, redis_db=self.redis_db)
+
+        result = self.process(img_one.value, img_two.value)
+
+        img_one.value = result[0]
+        self.output_image_one = Image.set_frame(img=img_one, package_uID=self.uID, redis_db=self.redis_db)
+
+        img_two.value = result[1]
+        self.output_image_two = Image.set_frame(img=img_two, package_uID=self.uID, redis_db=self.redis_db)
+
+        packageModel = build_response_mix(self)
+        return packageModel
+
+
+if "__main__" == __name__:
+    Executor(sys.argv[1]).run()
--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,106 +1,189 @@
-
 from pydantic import Field, validator
-from typing import List, Optional, Union, Literal
+from typing import List, Union, Literal, Optional
 from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
 
-
-class InputImage(Input):
-    name: Literal["inputImage"] = "inputImage"
+class InputImageOne(Input):
+    name: Literal["inputImageOne"] = "inputImageOne"
     value: Union[List[Image], Image]
     type: str = "object"
 
-    @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
-        if isinstance(value, Image):
+    @validator("type",pre=True, always=True)
+    def set_type_based_on_value(cls,value,values):
+        value=values.get('value')
+        if isinstance(value,Image):
             return "object"
-        elif isinstance(value, list):
+        elif isinstance(value,list):
             return "list"
+        return "object"
 
     class Config:
-        title = "Image"
+        title = "Image Input 1"
 
-
-class OutputImage(Output):
-    name: Literal["outputImage"] = "outputImage"
-    value: Union[List[Image],Image]
+class InputImageTwo(Input):
+    name: Literal["inputImageTwo"] = "inputImageTwo"
+    value: Union[List[Image], Image]
     type: str = "object"
 
-    @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
-        if isinstance(value, Image):
+
+    @validator("type",pre=True, always=True)
+    def set_type_based_on_value(cls,value,values):
+        value=values.get('value')
+        if isinstance(value,Image):
             return "object"
-        elif isinstance(value, list):
+        elif isinstance(value,list):
             return "list"
+        return "object"
 
     class Config:
-        title = "Image"
+        title = "Image Input 2"
 
+class OutputImageOne(Output):
+    name: Literal["OutputImageOne"] = "OutputImageOne"
+    value: Union[List[Image], Image]
+    type: str = "object"
 
-class KeepSideFalse(Config):
-    name: Literal["False"] = "False"
-    value: Literal[False] = False
-    type: Literal["bool"] = "bool"
-    field: Literal["option"] = "option"
+    @validator("type",pre=True, always=True)
+    def set_type_based_on_value(cls,value,values):
+        value=values.get('value')
+        if isinstance(value,Image):
+            return "object"
+        elif isinstance(value,list):
+            return "list"
+        return "object"
 
     class Config:
-        title = "Disable"
+        title = "Result Output Image 1"
 
+class OutputImageTwo(Output):
+    name: Literal["OutputImageTwo"] = "OutputImageTwo"
+    value: Union[List[Image], Image]
+    type: str = "object"
 
-class KeepSideTrue(Config):
+    @validator("type",pre=True, always=True)
+    def set_type_based_on_value(cls,value,values):
+        value=values.get('value')
+        if isinstance(value,Image):
+            return "object"
+        elif isinstance(value,list):
+            return "list"
+        return "object"
+
+    class Config:
+        title = "Result Output Image 2"
+
+class OptionAIntegerField(Config):
+    name: Literal["OptionAIntegerField"] = "OptionAIntegerField"
+    value: int = Field(default=10)
+    type: Literal["number"] = "number"
+    field: Literal["textInput"] = "textInput"
+
+    class Config:
+        title = "Integer Field for A"
+
+class BoolOptionTrue(Config):
     name: Literal["True"] = "True"
     value: Literal[True] = True
     type: Literal["bool"] = "bool"
     field: Literal["option"] = "option"
-
     class Config:
         title = "Enable"
 
+class BoolOptionFalse(Config):
+    name: Literal["False"] = "False"
+    value: Literal[False] = False
+    type: Literal["bool"] = "bool"
+    field: Literal["option"] = "option"
+    class Config:
+        title = "Disable"
 
-class KeepSideBBox(Config):
-    """
-        Rotate image without catting off sides.
-    """
-    name: Literal["KeepSide"] = "KeepSide"
-    value: Union[KeepSideTrue, KeepSideFalse]
+class OptionABoolField(Config):
+    name: Literal["OptionABoolField"] = "OptionABoolField"
+    value: Union[BoolOptionTrue, BoolOptionFalse]
     type: Literal["object"] = "object"
     field: Literal["dropdownlist"] = "dropdownlist"
 
     class Config:
-        title = "Keep Sides"
+        title = "Boolean Field for A"
+        json_schema_extra = {
+            "target": "value"
+        }
 
-
-class Degree(Config):
-    """
-        Positive angles specify counterclockwise rotation while negative angles indicate clockwise rotation.
-    """
-    name: Literal["Degree"] = "Degree"
-    value: int = Field(ge=-359.0, le=359.0,default=0)
-    type: Literal["number"] = "number"
-    field: Literal["textInput"] = "textInput"
-    placeHolder: Literal["[-359, 359]"] = "[-359, 359]"
+class OptionA(Config):
+    name: Literal["OptionA"] = "OptionA"
+    optionAIntegerField: OptionAIntegerField
+    optionABoolField: OptionABoolField
+    value: Literal["OptionA"] = "OptionA"
+    type: Literal["string"] = "string"
+    field: Literal["option"] = "option"
 
     class Config:
-        title = "Angle"
+        title = "Method A"
+
+class OptionBFloatField(Config):
+    name: Literal["OptionBFloatField"] = "OptionBFloatField"
+    value: float = Field(default=1.5)
+    type: Literal["number"] = "number"
+    field: Literal["textInput"] = "textInput"
+
+    class Config:
+        title = "Float Field for B"
+
+class OptionBStringField(Config):
+    name: Literal["OptionBStringField"] = "OptionBStringField"
+    value: str = Field(default="default_string")
+    type: Literal["string"] = "string"
+    field: Literal["textInput"] = "textInput"
+
+    class Config:
+        title = "String Field for B"
+
+class OptionB(Config):
+    name: Literal["OptionB"] = "OptionB"
+    optionBFloatField: OptionBFloatField
+    optionBStringField: OptionBStringField
+    value: Literal["OptionB"] = "OptionB"
+    type: Literal["string"] = "string"
+    field: Literal["option"] = "option"
+    class Config:
+        title = "Method B"
+
+class DemoDependentDropdown(Config):
+    name: Literal["DemoDependentDropdown"] = "DemoDependentDropdown"
+    value: Union[OptionA, OptionB]
+    type: Literal["object"] = "object"
+    field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
+    class Config:
+        title = "Select Method"
 
 
-class PackageInputs(Inputs):
-    inputImage: InputImage
 
 
-class PackageConfigs(Configs):
-    degree: Degree
-    drawBBox: KeepSideBBox
+class GrayExecutorInputs(Inputs):
+    inputImageOne: InputImageOne
+
+class MixExecutorInputs(Inputs):
+    inputImageOne: InputImageOne
+    inputImageTwo: InputImageTwo
 
 
-class PackageOutputs(Outputs):
-    outputImage: OutputImage
+class GrayExecutorConfigs(Configs):
+    demoDependentDropdown: DemoDependentDropdown
+
+class MixExecutorConfigs(Configs):
+    demoDependentDropdown: DemoDependentDropdown
+
+class GrayExecutorRequest(Request):
+    inputs: Optional[GrayExecutorInputs]
+    configs: GrayExecutorConfigs
+    class Config:
+        json_schema_extra = {
+            "target": "configs"
+        }
 
 
-class PackageRequest(Request):
-    inputs: Optional[PackageInputs]
-    configs: PackageConfigs
+class MixExecutorRequest(Request):
+    inputs: Optional[MixExecutorInputs]
+    configs: MixExecutorConfigs
 
     class Config:
         json_schema_extra = {
@@ -108,41 +191,56 @@ class PackageRequest(Request):
         }
 
 
-class PackageResponse(Response):
-    outputs: PackageOutputs
+class GrayExecutorOutputs(Outputs):
+    outputImageOne: OutputImageOne
 
+class MixExecutorOutputs(Outputs):
+    outputImageOne: OutputImageOne
+    outputImageTwo: OutputImageTwo
 
-class PackageExecutor(Config):
-    name: Literal["Package"] = "Package"
-    value: Union[PackageRequest, PackageResponse]
+class GrayExecutorResponse(Response):
+    outputs: GrayExecutorOutputs
+
+class MixExecutorResponse(Response):
+    outputs: MixExecutorOutputs
+
+class GrayExecutor(Config):
+    name: Literal["GrayExecutor"] = "GrayExecutor"
+    value: Union[GrayExecutorRequest, GrayExecutorResponse]
     type: Literal["object"] = "object"
     field: Literal["option"] = "option"
-
     class Config:
-        title = "Package"
+        title = "GrayExecutor"
         json_schema_extra = {
             "target": {
                 "value": 0
             }
         }
 
+class MixExecutor(Config):
+    name: Literal["MixExecutor"] = "MixExecutor"
+    value: Union[MixExecutorRequest, MixExecutorResponse]
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+    class Config:
+        title = "MixExecutor"
+        json_schema_extra = {
+            "target": {
+                "value": 0
+            }
+        }
 
 class ConfigExecutor(Config):
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-    value: Union[PackageExecutor]
+    value: Union[GrayExecutor, MixExecutor]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
 
     class Config:
         title = "Task"
-        json_schema_extra = {
-            "target": "value"
-        }
-
 
 class PackageConfigs(Configs):
     executor: ConfigExecutor
-
 
 class PackageModel(Package):
     configs: PackageConfigs
