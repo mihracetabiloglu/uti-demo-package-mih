d2e1f9998523f27eec14fa78a3b28e3c1377680f
Mihrace <mihracetabiloglu@gmail.com>
2026-08-01 20:10:56 +0300

    executor ilk hali

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,29 +1,15 @@
+
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import (
-    PackageModel, PackageConfigs, ConfigExecutor,
-    GrayExecutor, GrayExecutorResponse, GrayExecutorOutputs, OutputImageOne,
-    MixExecutor, MixExecutorResponse, MixExecutorOutputs, OutputImageTwo,
-)
+from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
 
-def build_response_gray(context):
-    outputImageOne = OutputImageOne(value=context.output_image_one)
-    outputs = GrayExecutorOutputs(outputImageOne=outputImageOne)
-    response = GrayExecutorResponse(outputs=outputs)
-    grayExecutor = GrayExecutor(value=response)
-    configExecutor = ConfigExecutor(value=grayExecutor)
-    packageConfigs = PackageConfigs(executor=configExecutor)
-    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
-    packageModel = package.build_model(context)
-    return packageModel
 
-def build_response_mix(context):
-    outputImageOne = OutputImageOne(value=context.output_image_one)
-    outputImageTwo = OutputImageTwo(value=context.output_image_two)
-    outputs = MixExecutorOutputs(outputImageOne=outputImageOne, outputImageTwo=outputImageTwo)
-    response = MixExecutorResponse(outputs=outputs)
-    mixExecutor = MixExecutor(value=response)
-    configExecutor = ConfigExecutor(value=mixExecutor)
-    packageConfigs = PackageConfigs(executor=configExecutor)
+def build_response(context):
+    outputImage = OutputImage(value=context.image)
+    Outputs = PackageOutputs(outputImage=outputImage)
+    packageResponse = PackageResponse(outputs=Outputs)
+    packageExecutor = PackageExecutor(value=packageResponse)
+    executor = ConfigExecutor(value=packageExecutor)
+    packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
     return packageModel
