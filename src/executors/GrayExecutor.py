f2976690f4f117d63a75b4f0efd37f7f76dc52f3
Mihrace <mihracetabiloglu@gmail.com>
2026-08-01 23:16:40 +0300

    grayexecutor düzenlemesi

--- src/executors/GrayExecutor.py
+++ src/executors/GrayExecutor.py
@@ -7,14 +7,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.Package.src.models.PackageModel import PackageModel
-from components.Package.src.utils.response import build_response_gray
+from components.DemoPackageMih.src.models.PackageModel import PackageModel
+from components.DemoPackageMih.src.utils.response import build_response_gray
+
 
 class GrayExecutor(Component):
     def __init__(self, request, bootstrap):
         super().__init__(request, bootstrap)
         self.request.model = PackageModel(**(self.request.data))
-        self.image_one = self.request.get_param("inputImageOne")
+
+        self.image = self.request.get_param("inputImageOne")
         self.method = self.request.get_param("demoDependentDropdown")
 
         if self.method == "OptionA":
@@ -28,18 +30,22 @@ class GrayExecutor(Component):
     def bootstrap(config: dict) -> dict:
         return {}
 
-    def process(self, img_one):
-        gray_image = cv2.cvtColor(img_one, cv2.COLOR_BGR2GRAY)
-        return gray_image
+    def process(self, img):
+        """
+        Görseli gri tona dönüştürür
+        """
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     def run(self):
-        img_one = Image.get_frame(img=self.image_one, redis_db=self.redis_db)
-        img_one.value  = self.process(img_one.value)
+        img = Image.get_frame(img=self.image, redis_db=self.redis_db)
 
-        self.output_image_one = Image.set_frame(img=img_one, package_uID=self.uID, redis_db=self.redis_db)
+        img.value = self.process(img.value)
+
+        self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
 
         packageModel = build_response_gray(context=self)
         return packageModel
+
 
 if "__main__" == __name__:
     Executor(sys.argv[1]).run()
